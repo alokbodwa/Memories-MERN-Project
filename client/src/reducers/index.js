07b7dafde077bf5dab5bc65ae4fefcc585527f2a
Alok Kumar <kralok@iitk.ac.in>
2021-06-26 17:53:37 +0530

    completed redux store. Fixed some bugs.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
+// redux store
 import { combineReducers } from "redux";
 import posts from './posts'
 
 export default combineReducers({
+    // posts reducer is in our store now and available to every child for use
     posts: posts
 })
