c031cbd680cd1a38988e83d5dbce325af5d90e6f
Alok Kumar <kralok@iitk.ac.in>
2021-06-26 14:15:02 +0530

    added redux files

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
 import memories from './images/memories.png'
+import Form from './components/Forms/Form'
+import useStyles from './styles'
 
 const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch()
+    }, [])
+
     return (
         <Container maxwidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories.png" height="360" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories.png" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
-                            <Posts />
+                            {/* <Posts /> */}
                         </Grid>
                         <Grid item xs={12} sm={4}>
                             <Form />
